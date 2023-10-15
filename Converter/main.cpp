5df8443736d8cc5b7687147e33a5c8f6aaa283c3
Michael <roesslma@miamioh.edu>
2023-10-14 23:06:53 -0400

    Can now interpret A-Z

--- Converter/main.cpp
+++ Converter/main.cpp
@@ -59,44 +59,108 @@ int process(char& val) {
     return rVal;
 }
 
-float x2dec(int from, std::string num) {
-    float sum = 0;
+char process2(std::string val) {
+    char rVal;
+    //std::cout << val << std::endl;
+    if      (val == "10"){rVal = 'A';}
+    else if (val == "11"){rVal = 'B';}
+    else if (val == "12"){rVal = 'C';}
+    else if (val == "13"){rVal = 'D';}
+    else if (val == "14"){rVal = 'E';}
+    else if (val == "15"){rVal = 'F';}
+    else if (val == "16"){rVal = 'G';}
+    else if (val == "17"){rVal = 'H';}
+    else if (val == "18"){rVal = 'I';}
+    else if (val == "19"){rVal = 'J';}
+    else if (val == "20"){rVal = 'K';}
+    else if (val == "21"){rVal = 'L';}
+    else if (val == "22"){rVal = 'M';}
+    else if (val == "23"){rVal = 'N';}
+    else if (val == "24"){rVal = 'O';}
+    else if (val == "25"){rVal = 'P';}
+    else if (val == "26"){rVal = 'Q';}
+    else if (val == "27"){rVal = 'R';}
+    else if (val == "28"){rVal = 'S';}
+    else if (val == "29"){rVal = 'T';}
+    else if (val == "30"){rVal = 'U';}
+    else if (val == "31"){rVal = 'V';}
+    else if (val == "32"){rVal = 'W';}
+    else if (val == "33"){rVal = 'X';}
+    else if (val == "34"){rVal = 'Y';}
+    else if (val == "35"){rVal = 'Z';}
+    else if (stoi(val) < 10){rVal = val[0];}
+    else {}
+    //std::cout << rVal << ",";
+    return rVal;
+}
+
+double x2dec(int from, std::string num) {
+    double sum = 0;
     int subtr = 0;
     for (int i = 0; i < num.length(); i++) {
         int x = process(num.at(num.length()-(i+1)));
         //std::cout << x << ", ";
+        // Check if digit
+        if (x >= from) {
+            std::cout << "Error in number value: " << num << std::endl;
+            std::cout << "Value '" << num.at(num.length()-(i+1)) << "' is invalid in base-" << from << std::endl;
+        }
         if (x >= 0) {
             sum += x * std::pow(from, i-subtr);
             //std::cout <<sum<<" ";
-        } else if (x == -1) {
-            sum *= -1;
+        } else if (x == -1) {// '-'
+            // add something to check '-' is used properly?
+            if ((i+1) == num.length()) {
+                sum *= -1;
+            } else {
+                std::cout << "Error in number value: " << num << std::endl;
+                std::cout << "Improper usage of '-'" << std::endl;
+                sum = 0;// 0 if error
+            }
+            
             break;
-        } else if (x == -2) {
+        } else if (x == -2) {// '.'
             if (subtr != 0) {
-                std::cout << "Error in number value." << std::endl;
+                std::cout << "Error in number value: " << num << std::endl;
+                std::cout << "Improper usage of '.'" << std::endl;
+                sum = 0;// 0 if error
                 break;
             }
+            //std::cout << sum << std::endl;
             sum = sum / std::pow(from, i);
+            //std::cout << sum << std::endl;
             subtr = i+1;
         } else if (x == -3) {
-            
+            std::cout << "Error in number value: " << num << std::endl;
+            std::cout << "Unknown character '" << num.at(num.length()-(i+1)) << "'" << std::endl;
+            sum = 0;// 0 if error
+            break;
         }
     }
-    //std::cout << std::endl;
+    //std::cout << sum << std::endl;
     return sum;
 }
 
 //divide by "to" until less than zero
 //maybe recursion
 //*MAYBE CHAR BY CHAR*
-std::string dec2x(int num, int to) {
+std::string dec2x(double num, int to) {//num should be float?
+    std::string digit, nextDigit = "", decDigit = "";
     int sum = 0;
     //float num = number;
-    float rem = num % to;
-    num = num - rem;
-    std::string digit, nextDigit = "";
-    while (num >= to) {
-        num = num - to;
+    int numI = num;//Find way to split at the decimal point?
+    /*if (num != numI) {
+        std::cout << num << " " << numI << " " << (num-numI) << std::endl;
+        int p = 1;
+        while ((num-numI)*p != (int)((num-numI)*p)) {
+            p *= 10;
+        }
+        decDigit = "." + dec2x((int)((num-numI)*10), to);
+    }*/
+    int rem = numI % to;
+    num = numI - rem;
+    while (numI >= to) {
+        numI = numI - to;
         sum++;
     }
     if (sum >= to) {
@@ -104,19 +168,48 @@ std::string dec2x(int num, int to) {
     } else if (sum != 0) {
         nextDigit = std::to_string(sum).substr(0,1);
     }
-    digit = std::to_string(rem).substr(0,1);
-    return nextDigit + digit;
+    //std::cout << rem << std::endl;
+    digit = process2(std::to_string(rem)/*.substr(0,1)*/);
+    //std::cout << digit << std::endl;
+    return nextDigit + digit + decDigit;
 }
 
 void userInput() {
     std::string num;
     int from, to;
     std::cout << "Type conversion as: " << std::endl;
+    std::cout << "[from] [value] [to]" << std::endl;
     while (true) {
         std::cout << "> ";
-        std::cin >> from >> num >> to;
-        //std::cout << x2dec(from, num) << std::endl;
-        std::cout << dec2x(x2dec(from, num), to) << std::endl;
+        if (!(std::cin >> from >> num >> to)) {
+            std::cin.clear();
+            std::cin.ignore(99999, '\n');
+            std::cout << "Error in input" << std::endl;
+            std::cout << "Type conversion as: " << std::endl;
+            std::cout << "[from] [value] [to]" << std::endl;
+            std::cout << "[from] is the base of the value to convert from (2-36)." << std::endl;
+            std::cout << "[value] is the number value to be converted." << std::endl;
+            std::cout << "[to] is the base to convert to (2-36)." << std::endl;
+        } else if (from < 2 || from > 36) {
+            std::cout << "Error in [from] value: " << from << std::endl;
+            std::cout << "[from] is the base of the value to convert from (2-36)." << std::endl;
+            /*if (from == "?") {
+                std::cout << "Type conversion as: " << std::endl;
+                std::cout << "[from] [value] [to]" << std::endl;
+                std::cout << "[from] is the base of the value to convert from (2-36)." << std::endl;
+                std::cout << "[value] is the number value to be converted." << std::endl;
+                std::cout << "[to] is the base to convert to (2-36)." << std::endl;
+            } else {
+                std::cout << "Error in [from] value: " << from << std::endl;
+                std::cout << "[from] is the base of the value to convert from (2-36)." << std::endl;
+            }*/
+        } else if (to < 2 || to > 36) {
+            std::cout << "Error in [to] value: " << to << std::endl;
+            std::cout << "[to] is the base to convert to (2-36)." << std::endl;
+        } else {
+            std::cout << dec2x(x2dec(from, num), to) << std::endl;
+        }
+        
     }
 }
 
